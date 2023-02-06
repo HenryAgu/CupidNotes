1554d4ad1fdc41e45b8afe7ee8f641141cea6947
Henry <aguhenrychuks@gmail.com>
2023-02-06 23:23:11 +0100

    feat: signup page updated

--- src/components/Pages/SignUp.js
+++ src/components/Pages/SignUp.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 // stylesheet
-import './SignUp.css';
+import "./SignUp.css";
+
+// NavLink
+import { Link } from "react-router-dom";
 
 // Helmet
 import { Helmet } from "react-helmet";
@@ -14,6 +17,32 @@ const SignUp = () => {
         <title> Login Page | Cupid Notes</title>
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
+      <div className="login sign_up">
+        <form action="" className="form">
+          <input type="text" placeholder="Choose your Username" required />
+          <input type="password" placeholder="Choose your Password" required />
+          <button>
+            <Link to="/" className="home_button">
+              Sign Up
+            </Link>
+          </button>
+        </form>
+        <p>
+          Already have an account?{" "}
+          <span>
+            <Link to="/" className="sign_up_link">
+              Login
+            </Link>
+          </span>
+        </p>
+        <div className="policy">
+          <input type="checkbox" name="" id="" />
+          <p>
+            Yes, I understand and agree to the <span>Wicked Terms of Service, </span>
+            including the <span>User Agreement</span> and <span>Privacy Policy</span>
+          </p>
+        </div>
+      </div>
     </>
   );
 };
